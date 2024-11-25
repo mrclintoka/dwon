e056668526c0c1f8f14f10ddaa1395d14d64eaf7
mrclintoka <okaclint@gmail.com>
2024-11-25 08:14:36 -1000

    &apos; error

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -11,7 +11,7 @@ export const Hero = () => {
                     <Image alt="sloth photo" className="rounded-full py-3 h-[300px] w-[300px] lg:h-[400px] lg:w-[400px]" src="/sloth.png" height={400} width={400} />
                 </div>
                 <div className="max-w-xl py-8 space-y-8 flex-col items-center justify-center text-center lg:text-end">
-                    <p className="text-4xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">I'm Dright Weyes</p>
+                    <p className="text-4xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">I&apos;m Dright Weyes</p>
                     <p className="text-gray-600">now is the present! I&apos;m a sloth, who wants to start a religion call &apos;ism&apos; The faith of being. Oh btw my coin is <span className="text-xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">$DWON</span>. The coin is not launched, because I rather build a community first, if you want to be more involved, reach out to drightweyes@gmail.com</p>
                     <div className="flex justify-center space-x-4 items-center lg:justify-end">
                         <FaDiscord color="gray" size={50}/>
